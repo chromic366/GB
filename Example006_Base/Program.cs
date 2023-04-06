4d786555fe4fed4c9d244cacf2cef44a704f5a80
Chromic <chromic366@gmail.com>
2023-04-06 23:11:38 +0500

    add Example006 Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 22;
+int b = 15;
+int c = 6;
+int d = 8;
+int e = 4;
+
+int max = a;
+
+if(b > max) max = b;
+if(c > max) max = c;
+if(d > max) max = d;
+if(e > max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
