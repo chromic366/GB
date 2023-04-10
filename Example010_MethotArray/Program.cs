5a970c98ce4676219e72053e24a69dea95f72e3a
Chromic <chromic366@gmail.com>
2023-04-10 22:48:58 +0500

    add Example010 MethotArray

---
+++ Example010_MethotArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {54, 12, 23, 4, 54, 23, 74, 8};
+
+int n = array.Length;
+int find = 23;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find) 
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
